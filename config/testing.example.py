f1934e344b4de0d543c135a37629561f1a40d357
Christian Hettlage <hettlage@saao.ac.za>
2016-05-05 19:33:42 +0200

    Use Flask-SQLAlchemy

--- config/testing.example.py
+++ config/testing.example.py
@@ -2,5 +2,5 @@ import os
 
 DEBUG = True
 SECRET_KEY = os.environ.get('SALTSTATS_TEST_SECRET_KEY')
-DATABASE_URI = os.environ.get('SALTSTATS_TEST_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = os.environ.get('SALTSTATS_TEST_DATABASE_URI')
 LDAP_SERVER = 'your.server.address'
--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,10 +4,12 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.login import LoginManager
+from flask.ext.sqlalchemy import SQLAlchemy
 
 logger = None
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -34,6 +36,7 @@ def create_app(config_name):
     logger = app.logger
 
     bootstrap.init_app(app)
+    db.init_app(app)
     login_manager.init_app(app)
 
     from .main import main as main_blueprint
