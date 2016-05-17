23aa178ffe40444993d2c63a667aa25c2318d5c7
Christian Hettlage <hettlage@saao.ac.za>
2016-05-17 17:26:28 +0200

    Simple dashboard page

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -1,26 +1,36 @@
 import datetime
 
-from bokeh.models import Range1d
-from bokeh.models.formatters import DatetimeTickFormatter
 from flask.ext.login import login_required
 from flask import render_template
-import numpy as np
-import pandas as pd
 
 from ..main import main
 from ..plot.block_visits import BlockVisitPlots
-from ..plot.plot import TimeBarPlot, DialPlot
+from ..plot.engineering_time import EngineeringTimePlots
+from ..plot.operation_efficiency import OperationEfficiencyPlots
+from ..plot.science_time import ScienceTimePlots
+from ..plot.shutter_open_efficiency import ShutterOpenEfficiencyPlots
+from ..plot.telescope_downtime import TelescopeDowntimePlots
+from ..plot.weather_downtime import WeatherDowntimePlots
 
 
 @main.route('/', methods=['GET'])
 @login_required
 def home():
-    # dp = DialPlot(values=[15, 24],
-    #               label_values=[0, 10, 20, 30],
-    #               label_color_func=lambda x : 'green' if x < 20 else 'red',
-    #               display_values=['15 %', '456'])
-    # return render_template('dummy.html', db=dp)
+    date = datetime.date(2016, 3, 29)
 
-    bp = BlockVisitPlots(datetime.date(2016, 4, 4))
-    plot = bp.monthly_plot(months=6)
-    return render_template('dummy.html', db=bp.daily_plot(31), mb=plot)
+    block_visits = BlockVisitPlots(date)
+    science_time = ScienceTimePlots(date)
+    weather_downtime = WeatherDowntimePlots(date)
+    telescope_downtime = TelescopeDowntimePlots(date)
+    engineering_time = EngineeringTimePlots(date)
+    shutter_open_efficiency = ShutterOpenEfficiencyPlots(date)
+    operation_efficiency = OperationEfficiencyPlots(date)
+
+    return render_template('dashboard.html',
+                           block_visits=block_visits,
+                           science_time=science_time,
+                           weather_downtime=weather_downtime,
+                           telescope_downtime=telescope_downtime,
+                           engineering_time=engineering_time,
+                           shutter_open_efficiency=shutter_open_efficiency,
+                           operation_efficiency=operation_efficiency)
