44127d9f2bf7af20195f301a656f83d5b83bf909
Christian Hettlage <hettlage@saao.ac.za>
2016-05-25 23:40:20 +0200

    View for mirror recoating statistics

--- src/app/main/views.py
+++ src/app/main/views.py
@@ -1,7 +1,10 @@
 import datetime
 
+from flask import flash, redirect, render_template, url_for
 from flask.ext.login import login_required
-from flask import render_template
+from flask.ext.wtf import Form
+from wtforms import FileField, SubmitField
+from wtforms.validators import DataRequired
 
 from ..main import main
 from ..plot.block_visits import BlockVisitPlots
@@ -11,6 +14,8 @@ from ..plot.science_time import ScienceTimePlots
 from ..plot.shutter_open_efficiency import ShutterOpenEfficiencyPlots
 from ..plot.telescope_downtime import TelescopeDowntimePlots
 from ..plot.weather_downtime import WeatherDowntimePlots
+
+from ..plot.mirror_recoating import MirrorRecoatingPlot, update_database
 
 
 @main.route('/', methods=['GET'])
@@ -34,3 +39,31 @@ def home():
                            engineering_time=engineering_time,
                            shutter_open_efficiency=shutter_open_efficiency,
                            operation_efficiency=operation_efficiency)
+
+
+class RecoatingForm(Form):
+    data_file = FileField('Excel spreadsheet', validators=[DataRequired()])
+    submit = SubmitField('Update Database')
+
+
+@main.route('/recoating', methods=['GET', 'POST'])
+@login_required
+def mirror_recoating():
+    """View for a mirror recoating plot, as well as a file upload form for updating the database.
+    """
+
+    form = RecoatingForm()
+    if form.validate_on_submit():
+        file = form.data_file.data
+        form.data_file.data = None
+        try:
+            update_database(file)
+        except BaseException as e:
+            flash('The database could not be updated: {message}'.format(message=e))
+        return redirect(url_for('main.mirror_recoating'))
+
+    now = datetime.date(2016, 4, 15) # datetime.datetime.now().date()
+    mirror_recoating = MirrorRecoatingPlot(now)
+
+    return render_template('recoating.html', mirror_recoating=mirror_recoating, form=form)
+
